5d72c590eb4e0584cda260a9a8bf89ec72756290
lisya-korn <charlisyagreely@gmail.com>
2026-04-27 20:30:58 +0700

    refactor: consolidate next config and add CSP headers

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '50mb',
+    },
+  },
   eslint: {
     // Membiarkan build tetap jalan meskipun ada error linting (seperti 'any')
     ignoreDuringBuilds: true,
@@ -9,7 +13,20 @@ const nextConfig: NextConfig = {
   typescript: {
     // Membiarkan build tetap jalan meskipun ada error tipe data
     ignoreBuildErrors: true,
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' *.vercel-scripts.com;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
